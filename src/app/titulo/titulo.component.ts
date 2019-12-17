5aac48fa525b95618626d4d372e54effd0ad236c
Javier Lete <javier@eskura21.com>
2019-12-17 19:37:56 +0100

    Evento ngOnInit y constructor

--- src/app/titulo/titulo.component.ts
+++ src/app/titulo/titulo.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-titulo',
     templateUrl: './titulo.component.html',
-    styleUrls: [ './titulo.component.css' ]
+    styleUrls: ['./titulo.component.css']
 })
-export class TituloComponent {
+export class TituloComponent implements OnInit {
     @Input() texto: string;
+
+    constructor() {
+        console.log('TituloComponent', 'CONSTRUIDO');
+    }
+
+    ngOnInit(): void {
+        console.log('TituloComponent', 'CARGADO');
+    }
 }

ec24180607d91941ef9b0bd34e509b04315e24b4
Javier Lete <javier@eskura21.com>
2019-12-18 20:18:44 +0100

    Ejemplo de directiva de atributo

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit {
 
-  constructor() { }
+  @Input('appHighlight') highlightColor: string;
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    // debugger;
+    // this.el.nativeElement.style.backgroundColor = 'yellow';
+    this.el.nativeElement.innerHTML = this.el.nativeElement.innerHTML.replace('ipsum', '*****');
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
